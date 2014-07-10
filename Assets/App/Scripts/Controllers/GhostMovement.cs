cfdb57b3c55a70e320de073d3adb9025f17ee4e4
Felix Izarra <paramaplexd@gmail.com>
2014-07-09 23:22:31 -0430

    Path

--- Assets/App/Scripts/Controllers/GhostMovement.cs
+++ Assets/App/Scripts/Controllers/GhostMovement.cs
@@ -3,45 +3,29 @@ using System.Collections;
 
 public class GhostMovement : MonoBehaviour {
 
-	public enum GhostType{
+	public enum GhostTypeMovement{
 		PingPongMovement,
 		CircularMovement
 	}
 
-	private Vector3 startPosition = new Vector3();
+	public string[] pathsNames;
 
-	public GameObject[] targetsPosition;
-	public float time ;
+	public int pathToUse;
 
-	public GhostType typeOfGhost;
+	public int time;
+	
+
+	public GhostTypeMovement typeOfMovement;
 
 	// Use this for initialization
 	public IEnumerator Start () {
-		startPosition = transform.localPosition;
 		yield return StartCoroutine(BeginMovement());
 	}
+	
 
 	public IEnumerator BeginMovement(){
-		int it = 0;
-		int augmenter = 1;
-		while(true){
-			if(targetsPosition.Length>0){
-				Debug.Log (it);
-				Vector3 actualTarget = targetsPosition[it].transform.localPosition;
-				iTween.MoveTo(this.gameObject, targetsPosition[it].transform.localPosition, time/targetsPosition.Length);
-				while(transform.localPosition != targetsPosition[it].transform.localPosition) yield return null;
-				Debug.Log("Next target "+it);
-				it+= augmenter;
-					
-				if(it == targetsPosition.Length || it == 0){
-					if(typeOfGhost == GhostType.CircularMovement) it = 0;
-					else {
-						augmenter = augmenter * -1;
-						it += augmenter; 
-					}
-				}
-			}
-		}
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(pathsNames[pathToUse]), "time", time));
+		yield return null;
 	}
 	
 
