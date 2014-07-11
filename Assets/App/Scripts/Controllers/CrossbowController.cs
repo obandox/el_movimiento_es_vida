74ea11750f992e9d6cc36f8392c06e68b5e2164e
MiguelObando <miguelobando@wattopoststudio.com>
2014-07-11 13:19:19 -0430

    * el movimiento xD

--- Assets/App/Scripts/Controllers/CrossbowController.cs
+++ Assets/App/Scripts/Controllers/CrossbowController.cs
@@ -6,25 +6,51 @@ public class CrossbowController : MonoBehaviour {
 	public float shootSpeede = 4.0f;
 	public Transform arrowPrefab;
 	public Transform crossBow;
+	public Transform cursor = null;
 	public Camera camera;
-
+	public float maxYAngle = 30.0f;
+	public float minYAngle = -0.0f;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetButtonDown("Fire1")){
-			var bullet = Instantiate(arrowPrefab,crossBow.transform.position,Quaternion.identity) as Transform;
-			var rigidBody = bullet.GetComponent<Rigidbody>();
-			if(rigidBody!=null)            
-				rigidBody.velocity = crossBow.transform.TransformDirection(new Vector3(0, 0,shootSpeede));
-
-		}
 		
 		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-		Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 100)) {
+			Vector3 targetPosition=new Vector3(hit.point.x, crossBow.position.y, hit.point.z);
+
+			crossBow.LookAt(targetPosition);
+
+			Vector3 eulerAngles = crossBow.eulerAngles;
+
+			if(eulerAngles.y > 180 && eulerAngles.y < 270)
+				eulerAngles.y = 270;
+			else if (eulerAngles.y < 180 &&  eulerAngles.y > 90)
+				eulerAngles.y = 90;
+
+			crossBow.eulerAngles = eulerAngles;
+
+			if(cursor!=null){
+				cursor.position = targetPosition;
+				cursor.LookAt(crossBow.position);
+			}
+
+			if(Input.GetButtonDown("Fire1")){
+
+				var bullet = Instantiate(arrowPrefab,crossBow.transform.position,Quaternion.identity) as Transform;
+				bullet.eulerAngles = crossBow.eulerAngles;
+				var rigidBody = bullet.GetComponent<Rigidbody>();
+				if(rigidBody!=null)            
+					rigidBody.velocity = crossBow.transform.TransformDirection(new Vector3(0, 0,shootSpeede));
+				
+			}
+
+
+		}
 	}
 }
