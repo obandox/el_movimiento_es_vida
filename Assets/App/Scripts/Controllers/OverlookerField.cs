a90c57b391d2cac9ae8089e9e6a6e7f0b4decb6b
Felix Izarra <paramaplexd@gmail.com>
2014-07-12 18:27:14 -0430

    Overlooker

---
+++ Assets/App/Scripts/Controllers/OverlookerField.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverlookerField : MonoBehaviour {
+	public string enterAction;
+	public string exitAction;
+	// Use this for initialization
+
+	public void OnTriggerEnter(Collider colli){
+		transform.parent.gameObject.SendMessage(enterAction);
+	}
+
+	public void OnTriggerExit(Collider colli){
+		transform.parent.gameObject.SendMessage(exitAction);
+	}
+}
---
+++ Assets/OverlookerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverlookerController : MonoBehaviour {
+
+	public GameObject Player {get; set;}
+
+	private bool looking = false;
+
+	private bool following = false;
+
+	public GameObject myGhost;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(looking) myGhost.transform.LookAt(Player.transform.position);
+
+		if(following) myGhost.transform.Translate(Vector3.forward*speed*Time.deltaTime);
+	}
+
+	public void Look(){
+		looking = true;
+	}
+
+	public void Follow(){
+		following = true;
+	}
+}
