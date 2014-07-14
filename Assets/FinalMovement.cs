877ea43430690ddefd2153cd5499fdd5aacc4fb8
Felix Izarra <paramaplexd@gmail.com>
2014-07-13 21:18:02 -0430

    Escenas

---
+++ Assets/FinalMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalMovement : MonoBehaviour {
+
+	public float time;
+	// Use this for initialization
+	void Start () {
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("intro"), "time", time, "easetype", iTween.EaseType.linear));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/MenuControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuControl : MonoBehaviour {
+	public string goTO = "Prueba01";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space)){
+			AutoFade.LoadLevel(goTO, 1f, 0.5f, Color.black);
+		}
+	}
+}
