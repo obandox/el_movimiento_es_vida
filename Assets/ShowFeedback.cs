37fffff91e40a8f4f9a9a94e005d30921f2c180b
Felix Izarra <paramaplexd@gmail.com>
2014-07-14 01:22:52 -0430

    Last Message

--- Assets/ShowFeedback.cs
+++ Assets/ShowFeedback.cs
@@ -22,8 +22,13 @@ public class ShowFeedback : MonoBehaviour {
 	}
 
 	public IEnumerator FeedbackWorker(){
+		if(SoulController.Shared.SoulboxCount<7){
 		texto.text = "Cajas de alma: "+SoulController.Shared.SoulboxCount+"/7";
 		shadow.text = texto.text;
+		}else{
+			texto.text = "Solo falta ir al templo";
+			shadow.text = texto.text;
+		}
 		yield return new WaitForSeconds(2f);
 		texto.text = "";
 		shadow.text = "";
